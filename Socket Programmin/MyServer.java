ee7d0733f29e8fc1e36bd5adc5552cf5688c9619
Pranav Kulkarni <66161105+pranavkulkarni2905@users.noreply.github.com>
2023-04-05 13:02:43 +0530

    Update MyServer.java

--- Socket Programmin/MyServer.java
+++ Socket Programmin/MyServer.java
@@ -2,23 +2,36 @@
     import java.io.*;  
     class MyServer{  
 		public static void main(String args[])throws Exception{  
+			
+			//created object of serversocket class and passed one port number port number must be onwards 1024
 			ServerSocket ss=new ServerSocket(3333);  
+			
+			//accepting request sent by client
 			Socket s=ss.accept();  
+			
+			//created object for output and input class
 			DataInputStream din=new DataInputStream(s.getInputStream());  
 			DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
 			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
 			  
 			String str="",str2="";  
-			while(!str.equals("stop")){  
 			
+			//terminates when client says "stop"
+			while(!str.equals("stop")){  
+				//read the message sent by client
 				str=din.readUTF();  
+				//print the message sent by client
 				System.out.println("client says: "+str);  
+			
 				str2=br.readLine();  
+				
+				//write the response to the client
 				dout.writeUTF(str2);  
 				dout.flush();  
 			}  
 		din.close();  
 		s.close();  
+			//close the connection
 		ss.close();  
     }
 }  
