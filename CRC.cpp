0461facef1da475d74e6a0002a42c3ce72928a1b
Pranav Kulkarni <66161105+pranavkulkarni2905@users.noreply.github.com>
2023-04-18 22:45:27 +0530

    Update CRC.cpp

--- CRC.cpp
+++ CRC.cpp
@@ -1,74 +1,177 @@
- #include<iostream>
+#include <iostream>
 using namespace std;
-
-void division(int temp[],int gen[],int n,int r)
-{
- for(int i=0;i<n;i++)
- {
-     if (gen[0]==temp[i])
-     {
-         for(int j=0,k=i;j<r+1;j++,k++)
-             if(!(temp[k]^gen[j]))
-                 temp[k]=0;
-             else
-                 temp[k]=1;
-     } 
- }
-}
-
 int main()
 {
-    
-int n,r,message[50],gen[50],temp[50];
- cout<<"Enter the number of message bits : ";
- cin>>n;
- cout<<"Enter the number of generator bits : ";
- cin>>r;
- 
- cout<<"Enter the message : ";
- for(int i=0;i<n;i++)
-     cin>>message[i];
- cout<<"Enter the generator : ";
- for(int i=0;i<r;i++)
-     cin>>gen[i];
- r--;
- 
- for(int i=0;i<r;i++)
-     message[n+i] = 0;
- for(int i=0;i<n+r;i++)
-     temp[i] = message[i];
- division(temp,gen,n,r);
- cout<<"CRC : ";
- for(int i=0;i<r;i++)
- {
-     cout<<temp[n+i]<<" ";
-     message[n+i] = temp[n+i];
- }
- cout<<endl<<"Transmitted Message : ";
- for(int i=0;i<n+r;i++)
-     cout<<message[i]<<" ";
+    int i,j,k,l;
      
- cout<<endl<<endl<<"At Receiver's End "<<endl;
- 
- cout<<"Enter the received message : ";
- 
- for(int i=0;i<n+r;i++)
-     cin>>message[i];
+    //Get Frame
+    int fs;
+    cout<<"\n Enter Size of data: ";
+    cin>>fs;
      
- for(int i=0;i<n+r;i++)
-     temp[i] = message[i];
+    int f[20];
      
- division(temp,gen,n,r);
+    cout<<" Enter data:";
+    for(i=0;i<fs;i++)
+    {
+        cin>>f[i];
+    }
  
- for(int i=0;i<r;i++)
- {
-     if(temp[n+i])
-     {
-         cout<<"Error detected messsage discarded";
-         return 0;
-     } }
- cout<<"No error .\nReceived Message : ";
- for(int i=0;i<n;i++)
-     cout<<message[i]<<" ";
- return 0;
+    //Get Generator
+    int gs;
+    cout<<"\n Enter key size: ";
+    cin>>gs;
+     
+    int g[20];
+     
+    cout<<"\n Enter key:";
+    for(i=0;i<gs;i++)
+    {
+        cin>>g[i];
+    }
+ 
+    cout<<"\n Sender Side:";
+    cout<<"\n data: ";
+    for(i=0;i<fs;i++)
+    {
+        cout<<f[i];
+    }
+    cout<<"\n key :";
+    for(i=0;i<gs;i++)
+    {
+        cout<<g[i];
+    }
+ 
+    //Append 0's
+    int rs=gs-1;
+    cout<<"\n Number of 0's to be appended: "<<rs;
+    for (i=fs;i<fs+rs;i++)
+    {
+        f[i]=0;
+    }
+ 
+    int temp[20];
+    for(i=0;i<20;i++)
+    {
+        temp[i]=f[i];
+    }
+ 
+    cout<<"\n Message after appending 0's :";
+    for(i=0; i<fs+rs;i++)
+    {
+        cout<<temp[i];
+    }
+ 
+    //Division
+    for(i=0;i<fs;i++)
+    {
+        j=0;
+        k=i;
+        //check whether it is divisible or not
+        if (temp[k]>=g[j])
+        {
+            for(j=0,k=i;j<gs;j++,k++)
+            {
+                if((temp[k]==1 && g[j]==1) || (temp[k]==0 && g[j]==0))
+                {
+                    temp[k]=0;
+                }
+                else
+                {
+                    temp[k]=1;
+                }
+            }
+        }
+    }
+ 
+    //CRC
+    int crc[15];
+    for(i=0,j=fs;i<rs;i++,j++)
+    {
+        crc[i]=temp[j];
+    }
+ 
+    cout<<"\n CRC bits: ";
+    for(i=0;i<rs;i++)
+    {
+        cout<<crc[i];
+    }
+ 
+    cout<<"\n Transmitted Frame: ";
+    int tf[15];
+    for(i=0;i<fs;i++)
+    {
+        tf[i]=f[i];
+    }
+    for(i=fs,j=0;i<fs+rs;i++,j++)
+    {
+        tf[i]=crc[j];
+    }
+    for(i=0;i<fs+rs;i++)
+    {
+        cout<<tf[i];
+    }
+ 
+    cout<<"\n Receiver side : ";
+    cout<<"\n Received Frame: ";
+    for(i=0;i<fs+rs;i++)
+    {
+        cout<<tf[i];
+    }
+ 
+    for(i=0;i<fs+rs;i++)
+    {
+        temp[i]=tf[i];
+    }
+ 
+    //Division
+    for(i=0;i<fs+rs;i++)
+    {
+        j=0;
+        k=i;
+        if (temp[k]>=g[j])
+        {
+            for(j=0,k=i;j<gs;j++,k++)
+            {
+                if((temp[k]==1 && g[j]==1) || (temp[k]==0 && g[j]==0))
+                {
+                    temp[k]=0;
+                }
+                else
+                {
+                    temp[k]=1;
+                }
+            }
+        }
+    }
+ 
+    cout<<"\n Remainder: ";
+    int rrem[15];
+    for (i=fs,j=0;i<fs+rs;i++,j++)
+    {
+        rrem[j]= temp[i];
+    }
+    for(i=0;i<rs;i++)
+    {
+        cout<<rrem[i];
+    }
+ 
+    int flag=0;
+    for(i=0;i<rs;i++)
+    {
+        if(rrem[i]!=0)
+        {
+            flag=1;
+        }
+    }
+ 
+    if(flag==0)
+    {
+        cout<<"\n Since Remainder Is 0 Hence Message Transmitted From Sender To Receriver Is Correct";
+    }
+    else
+    {
+        cout<<"\n Since Remainder Is Not 0 Hence Message Transmitted From Sender To Receriver Contains Error";
+    }
+return 0;
 }
